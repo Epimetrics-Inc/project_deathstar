5616b89c2bcfe2ae007e60e0046bc4f173e3b29f
mgmalana <mgmalana@gmail.com>
2017-09-06 13:30:23 +0800

    Merge branch 'UX'

--- Vagrantfile
+++ Vagrantfile
@@ -12,7 +12,7 @@ Vagrant.configure("2") do |config|
 
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://vagrantcloud.com/search.
-  config.vm.box = "bento/ubuntu-16.04"
+  config.vm.box = "deathstar"
 
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
@@ -23,8 +23,7 @@ Vagrant.configure("2") do |config|
   # within the machine from a port on the host machine. In the example below,
   # accessing "localhost:8080" will access port 80 on the guest machine.
   # NOTE: This will enable public access to the opened port
-  config.vm.network "forwarded_port", guest: 8000, host: 8000
-  config.vm.network "forwarded_port", guest: 5432, host: 5432
+  # config.vm.network "forwarded_port", guest: 80, host: 8080
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine and only allow access
@@ -45,7 +44,8 @@ Vagrant.configure("2") do |config|
   # the path on the guest to mount the folder. And the optional third
   # argument is a set of non-required options.
   # config.vm.synced_folder "../data", "/vagrant_data"
-
+  config.ssh.username = "vagrant"
+  config.ssh.password = "vagrant"
   # Provider-specific configuration so you can fine-tune various
   # backing providers for Vagrant. These expose provider-specific options.
   # Example for VirtualBox:
---
+++ web/pages/visualization.vue
@@ -0,0 +1,113 @@
+<template>
+    <div id="wrapper">
+
+        <!-- Navigation -->	
+        <navheader isVisActive="true" v-on:tooglesidebar="sidebarCollapse=!sidebarCollapse"></navheader>
+    		<collapse class="navbar-default navbar-collapse sidebar" v-model="sidebarCollapse">
+            <div class="sidebar-nav">
+                
+                <div class="sidebar-vis-options">
+                    <div class="form-group form-inline">
+                        <label>Type</label>
+                        <select class="form-control">
+                            <option>Word Cloud</option>
+                            <option>Frequency Chart</option>
+                        </select>
+                    </div>
+                    <div class="form-group form-inline">
+                        <label>Coverage</label>
+                        <select class="form-control" v-model="coverage">
+                            <option value="single">Specific Document</option>
+                            <option value="theme">Specific Theme</option>
+                            <option value="all">All Documents</option>
+                        </select>
+                    </div>
+                    <transition name="fade">
+                    <div class="form-group form-inline" v-show="coverage=='theme'">
+                      <label>Themes</label>
+                      <select class="form-control">
+                          <option>Adolescent Health</option>
+                          <option>Geriatric Health</option>
+                          <option>MNCHN</option>
+                          <option>Special Population</option>
+                      </select>
+                		</div>
+                		</transition>
+                </div>
+		        		
+		        		<transition name="fade">
+        					<navsearch is-doc-active="false" v-show="coverage=='single'"></navsearch>
+								</transition>
+                <div>
+                    <button class="btn btn-default visualize-button" href="#" type="button">
+                        Visualize
+                    </button>
+                </div>
+            </div>
+            <!-- /.sidebar-collapse -->
+        </collapse>
+            <!-- /.navbar-static-side -->
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+</template>
+
+<script>
+import 'vue-awesome/icons/search'
+import 'vue-awesome/icons/filter'
+import 'vue-awesome/icons/check'
+
+import navheader from '~/components/navheader.vue'
+import navsearch from '~/components/navsearch.vue'
+import dropdown from 'uiv/src/components/dropdown/Dropdown.vue'
+import collapse from 'uiv/src/components/collapse/Collapse.vue'
+import icon from 'vue-awesome/components/Icon'
+
+export default {
+  components: {
+    navheader,
+    navsearch,
+    dropdown,
+    collapse,
+    icon
+  },
+  data: function () {
+    return {
+      sidebarCollapse: true,
+      coverage: 'single'
+    }
+  }
+}
+</script>
+
+<style>
+/*Start of Visualisation Sidebar*/
+.sidebar .sidebar-vis-options {
+  margin-bottom: 0;
+}
+
+.sidebar .sidebar-vis-options *:last-child{
+  margin-bottom:0;
+}
+
+.sidebar .sidebar-vis-options .form-group > label {
+  width:28%;
+}
+
+.sidebar .sidebar-vis-options .form-group > select {
+  width:70%;
+}
+
+.sidebar .visualize-button{
+  float:right;
+}
+/*End of Visualisation Sidebar*/
+</style>
